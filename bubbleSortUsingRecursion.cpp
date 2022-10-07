da922830ebc20268363675018c851ecb8360a7e4
Vipan Kumar <108624219+VipanKumar01@users.noreply.github.com>
2022-10-07 02:59:53 -0700

    Update bubbleSortUsingRecursion.cpp

--- bubbleSortUsingRecursion.cpp
+++ bubbleSortUsingRecursion.cpp
@@ -2,18 +2,57 @@
 #include <iostream>
 using namespace std;
 
+void swap(int* a, int* b)
+{
+    int temp;
+    
+    // Swapping
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void bubbleSortUsingRecursion(int arr[], int n)
 {
+    // Base Condition
     if (n==1)
-        return 1;
+        return;
         
+    // If Array has only one Element then it is most Compulsury to count the element
     int count=1;
     
-    for(int i=0;)
+    // for Loop Traverse the Elements
+    for(int i=0;i<n;i++)
+    {
+        if (arr[i]>arr[i+1])
+        {
+            swap(&arr[i],&arr[i+1]);
+            count++;
+        }
+    }
+    
+    // Checking the Elements
+    if (count==0)
+        return;
+    
+    bubbleSortUsingRecursion(arr,n-1);
 }
-int main() {
-    // Write C++ code here
-    std::cout << "Hello world!";
 
+void displayArray(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+        cout<<endl;
+    }
+}
+
+
+int main() {
+    int arr[] = {54,34,2,54,67,99,567,111};
+    int size = sizeof(arr)/sizeof(int);
+    
+    bubbleSortUsingRecursion(arr,size);
+    displayArray(arr,size);
     return 0;
 }
